01827848962f954edf9d8fe197e18b29653129f8
Manuel Wegria <abstract.s33d@gmail.com>
2018-08-13 18:54:49 +0200

    add dependent destroy on booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :vessel
-  has_many :selected_options
+  has_many :selected_options, dependent: :destroy
 
   validates :start_date, presence: true
   validates :end_date, presence: true
