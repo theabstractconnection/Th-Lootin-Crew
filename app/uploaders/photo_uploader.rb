a444c3ec7a264f2f5393f7005be7830fea1dd1e7
Theophane Douthe <theophanedouthe@yahoo.fr>
2018-08-14 17:01:28 +0200

    vessel upload images done for view and show - missing validations et lat/lng auto generation

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,21 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :card do
+    resize_to_fit 256, 256
+  end
+
+  version :card_wide do
+    cloudinary_transformation gravity: :center, width: 800, height: 800, crop: :fill
+  end
+
+  version :avatar do
+    cloudinary_transformation effect: "brightness:30", radius: 20,
+      width: 150, height: 150, crop: :thumb, gravity: :face
+  end
+
 end
