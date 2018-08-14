a3fb4f48c6d3f775b95df7eb2c066ebb19861acd
Theophane Douthe <theophanedouthe@yahoo.fr>
2018-08-14 12:21:29 +0200

    vessels migrated with photo lat lng

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_13_185250) do
+ActiveRecord::Schema.define(version: 2018_08_14_101943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,9 @@ ActiveRecord::Schema.define(version: 2018_08_13_185250) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
+    t.float "lat"
+    t.float "lng"
     t.index ["user_id"], name: "index_vessels_on_user_id"
   end
 
