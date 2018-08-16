153d37cfacbcac68ea667cc57abf92a655733c33
s33d <abstract.s33d@gmail.com>
2018-08-16 15:30:27 +0200

    Update map.js

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -7,30 +7,9 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
 
-  // markers.forEach((marker) => {
-  //   var contentString = '<%= link_to vessel_path(vessel) do %>' +
-  // '<div id="content">' +
-  //     '<div class="content-title">' +
-  //     '<%= marker[:title] %>' +
-  //     '</div>' +
-  //     '<div class="content-img" style="background-image: linear-gradient(-225deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.5) 50%), url(<%= marker[:photo].url(:card) %>)";></div>' +
-  //     '</div>' +
-  //     '<% end %>'
-
-  //   var infowindow = new google.maps.InfoWindow({
-  //   content: contentString
-  //   });
-
-  //   marker.addListener('click', function() {
-  //   infowindow.open(map, marker);
-  //   });
-
-  // })
-
   if (markers.length === 0) {
     map.setZoom(2);
   } else if (markers.length === 1) {
-    console.log("hello");
     map.setCenter(markers[0].lat, markers[0].lng);
     map.setZoom(5);
   } else {
